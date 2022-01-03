122b1730716aaf8fe24022dbc7cb4adcc284d6f3
songsihyeon <s20040@gsm.hs.kr>
2022-01-03 23:37:22 +0900

    notify 기능 구현 이거 어케함 ?

--- notification/src/App.js
+++ notification/src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const useNotification = (title, options) => {
+  if (!Notification) return;
+
+  const fireNoti = () => {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    } else {
+      new Notification(title, options);
+    }
+  };
+  return fireNoti;
+};
 
 function App() {
+  const requestPermission = () => {
+    if (!Notification) {
+      return;
+    } else {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "denied") {
+          alert("알림을 허용하지 않았습니다.");
+        } else if (permission === "granted") {
+          alert("알림을 허용했습니다.");
+        }
+      });
+    }
+  };
+
+  const showNotification = () => {
+    if (Notification.permission === "granted") {
+      console.log("h");
+      let notify = new Notification("알림이 왔습니다.", {
+        body: "안녕하세요. \n알림을 성공적으로 수신했습니다.",
+        icon: "https://tistory3.daumcdn.net/tistory/2979840/attach/6e5d2d16ab6a49628dfe1f4c164e38a0",
+        tag: "메시지",
+      });
+      notify.onclick = function () {
+        alert(this.tag);
+      };
+    } else {
+      alert("알림을 허용해 주세요.");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={requestPermission}>권한요청</button>
+      <button onClick={showNotification}>알림</button>
+    </>
   );
 }
 
